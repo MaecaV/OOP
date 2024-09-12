9617d2e358cc8176a2e76836a1ea353d7813ba38
MaecaV <maecavinson@gmail.com>
2024-09-12 15:20:57 +0800

    Add files via upload

---
+++ LA#1 OOP.py
@@ -0,0 +1,10 @@
+class hero:
+    name = "Granger"
+    hp = 750
+    dmg = 50
+    basic_attack = 50
+marksman = hero()
+print(marksman.name)
+print(marksman.hp)
+print(marksman.dmg)
+print(marksman.basic_attack)   
