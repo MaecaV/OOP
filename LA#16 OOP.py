d775535b166d35bb8db0d714967b05fe3bb72dc8
MaecaV <maecavinson@gmail.com>
2024-09-30 17:55:32 +0800

    Create LA#16 OOP.py

---
+++ LA#16 OOP.py
@@ -0,0 +1,31 @@
+class Appliance:
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.model = model
+    def operate(self):
+        print("Operating")
+
+class WashingMachine(Appliance):
+    def __init__(self, name, model):
+        super().__init__(name, model)
+    def operate(self):
+        print("Washing clothes!")
+
+class Refrigerator(Appliance):
+    def __init__(self, name, model):
+        super().__init__(name, model)
+    def operate(self):
+        print("Keeping food cold!")
+
+class Microwave(Appliance):
+    def __init__(self, name, model):
+        Appliance.__init__(self, name, model)
+    def operate(self):
+        print("Heating food!")
+
+washingmachine= WashingMachine("WashingMachine", "Samsung")
+refrigerator = Refrigerator("Refrigerator", "Samsung")
+microwave = Microwave("Microwave", "Samsung")
+
+for appliance in [washingmachine, refrigerator, microwave]:
+    appliance.operate()
