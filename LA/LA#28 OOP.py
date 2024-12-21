d0dd7e59028f0c2fb95b5360419b1c7bcb36a4e2
MaecaV <maecavinson@gmail.com>
2024-12-21 23:35:26 +0800

    Delete LA directory

--- LA/LA#28 OOP.py
+++
@@ -1,9 +0,0 @@
-import tkinter
-
-your_name = "VERGARA_Maeca"
-window_ko = tkinter.Tk()
-window_ko.title(f"OOP LA28 {your_name}")
-window_ko.geometry("500x500")
-window_ko.configure(bg="green")
-
-window_ko.mainloop()
