d8bbe1b44ce2c330333f15761381afce82a7321a
MaecaV <maecavinson@gmail.com>
2024-12-21 22:35:23 +0800

    Delete LA#3 OOP.py

--- LA#3 OOP.py
+++
@@ -1,9 +0,0 @@
-class hero:
-    def __init__(self, name):
-        self.name = name
-
-    def describe(self, role):
-        print(f"{self.name} is a {role} hero.")
-
-ml = hero("Miya")
-ml.describe("Marksman")
