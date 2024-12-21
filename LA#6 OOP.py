e415ff37fd8b462050c7baf85a9a6ab94c6acd90
MaecaV <maecavinson@gmail.com>
2024-12-21 22:34:09 +0800

    Delete LA#6 OOP.py

--- LA#6 OOP.py
+++
@@ -1,9 +0,0 @@
-class Laptop():
-    def __init__(self, brand, model):
-        pass
-    def laptop_info(self):
-        pass
-Laptop = Laptop("Dell", "XPS 15")
-print(Laptop.laptop_info())
-
-    
